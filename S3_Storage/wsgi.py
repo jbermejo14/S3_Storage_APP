2939a2e0f521288125bb55bf87d005ac4599c994
jbermejo14 <jpbermejobramley@gmail.com>
2024-06-19 16:13:18 +0100

    First commit

---
+++ S3_Storage/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for S3_Storage project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'S3_Storage.settings')
+
+application = get_wsgi_application()
---
+++ S3_StorageApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class S3StorageappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'S3_StorageApp'
