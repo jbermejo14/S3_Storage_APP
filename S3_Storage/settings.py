012735d1ba23462f4582aff1dc7b64c08e535444
jbermejo14 <jpbermejobramley@gmail.com>
2024-06-20 11:24:40 +0100

    Added Docker

--- S3_Storage/settings.py
+++ S3_Storage/settings.py
@@ -144,8 +144,8 @@ STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 
 # Static files (CSS, JavaScript, Images)
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 # Media files
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
